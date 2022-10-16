efbf543b6af985954c5b7e07c9f334efc525b0d9
Jonathan Lo <72477459+jonathanlo411@users.noreply.github.com>
2022-10-16 11:34:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 // @match        https://canvas.ucsd.edu/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ucsd.edu
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function() {
